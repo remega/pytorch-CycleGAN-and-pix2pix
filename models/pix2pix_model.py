fafad6f0385681d642cf8981385ba1779f2f5209
Lai Jiang <jianglai.china@aliyun.com>
2019-07-30 06:25:19 +0800

    onlyA

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -57,8 +57,11 @@ class Pix2PixModel(BaseModel):
                                       not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
 
         if self.isTrain:  # define a discriminator; conditional GANs need to take both input and output images; Therefore, #channels for D is input_nc + output_nc
-            self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
+            # self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
+            #                               opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+            self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
                                           opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+#re
 
         if self.isTrain:
             # define loss functions
